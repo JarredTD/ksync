063fc828b621a8dd2023787a9e3f18827ebe6af4
JarredTD <87108293+JarredTD@users.noreply.github.com>
2024-09-09 08:19:25 -0700

    feat: Create init directory on command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use clap::{CommandFactory, Parser, Subcommand};
+use std::fs;
+use std::{io::Error, path::PathBuf};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Init {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Some(Commands::Init {}) => {
+            if let Ok(current_dir) = std::env::current_dir() {
+                match create_file(current_dir) {
+                    Ok(()) => println!("ksync directory initiated"),
+                    Err(ref e) if e.kind() == std::io::ErrorKind::AlreadyExists => {
+                        eprintln!("ksync already initialized")
+                    }
+                    Err(e) => eprintln!("error creating init directory: {}", e),
+                };
+            }
+        }
+        None => {
+            Args::command().print_help().unwrap();
+            println!();
+        }
+    }
+}
+
+fn create_file(directory_path: PathBuf) -> Result<(), Error> {
+    let directory_name = directory_path.join(".ksync");
+    let mut _directory = fs::create_dir(directory_name)?;
+    Ok(())
 }
